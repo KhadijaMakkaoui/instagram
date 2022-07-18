ef868532b223c478b95dcb6191f8f7829a5e972a
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-07-18 12:45:29 +0100

    Changer la font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/profile.jpg" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="">
+                <h1>makkaoui.khadija</h1>
             </div>
+           <div class="d-flex justify-content-between w-50">
+                <div class="pr-5">
+                    <strong>1</strong> publication
+                </div>
+                <div class="pr-5">
+                    <strong>61</strong> abonnés
+                </div>
+                <div class="pr-5">
+                    <strong>351</strong> abonnements
+                </div>
+           </div>
+           <div class="pt-4 fw-bold">Khadija Makkaoui</div>
+           <div class="">Hey! I am Khadija from Marrakesh, Morocco</div>
+           <div class="">
+            <a href="">https://github.com/KhadijaMakkaoui</a>
+           </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="" alt="">
         </div>
     </div>
 </div>
